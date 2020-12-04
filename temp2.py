97a23110e643738d382aeb5986b398d3e97ec3c1
Cam Cunningham <camcunningham188@gmail.com>
2020-12-03 20:36:12 -0500

    Now defaults to player vs AI

--- temp2.py
+++ temp2.py
@@ -14,7 +14,7 @@ def play(human=True, n=10000):
 
     while not game.isTerminalState(state):
         print(game.pretty_state(state, False))
-        if False:
+        if True:
             prompt = 'Choose a move, choices are %s: ' % (game.actions(state),)
             success = False
             while not success:
