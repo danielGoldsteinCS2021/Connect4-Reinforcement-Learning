911a331706dcae8203471dcaba27c554c75762eb
Cam Cunningham <camcunningham188@gmail.com>
2020-12-06 19:18:10 -0500

    new test cases

--- test_connect4_MCTS.py
+++ test_connect4_MCTS.py
@@ -8,9 +8,38 @@ class TestConnect4:
     def test_diag1(self):
         self.setUp()
         state = (('x'), ('o', 'x'), ('o', 'o', 'x'), ('o', 'o', 'o', 'x'), (), (), ())
-        assert(self.game.isGameOverDiag(state))
+        print(self.game.pretty_state(state))
+        print(self.game.isGameOverDiag(state))
 
     def test_diag2(self):
         self.setUp()
         state = ((), (), (), ('o', 'o', 'o', 'x'), ('o', 'o', 'x'), ('o', 'x'), ('x'))
-        assert(self.game.isGameOverDiag(state))
+        print(self.game.pretty_state(state))
+        print(self.game.isGameOverDiag(state))
+
+    def test_diag3(self):
+        self.setUp()
+        state = (('o'),(),('o'),('o','o','x','x'),('x','o','x'),('x','x'),('x','o','o','x','o'))
+        print(self.game.pretty_state(state))
+        print(self.game.isGameOverDiag(state))
+
+    def test_diag4(self):
+        self.setUp()
+        state = (('o'),('x','o'),('o','x','o'),('o','o','x','o'),('x','o','x'),('x','x'),('x','o'))
+        print(self.game.pretty_state(state))
+        print(self.game.isGameOverDiag(state))
+
+test = TestConnect4()
+# test.test_diag1()
+# test.test_diag2()
+# test.test_diag3()
+test.test_diag4()
+
+# test = TestConnect4()
+# game = connect4_MCTS.ConnectFour()
+# state = (('o'),(),('o'),('o','o','x','x'),('x','o','x'),('x','x'),('x','o','o','x','o'))
+# print(game.isGameOverDiag(state))
+# game = connect4_MCTS.ConnectFour()
+# state = (('o'),(),('o'),('o','o','x','x'),('x','o','x'),('x','x'),('x','o','o','x','o'))
+# print(game.pretty_state(state))
+# print(game.isGameOverDiag(state))
